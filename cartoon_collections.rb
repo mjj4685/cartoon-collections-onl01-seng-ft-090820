707e6af99f68702cb44db21774d6b92082cb57ea
MJ Johnson <mjohnson4685@gmail.com>
2020-09-14 03:46:13 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)  # code an argument here
+  dwarves_are_not_elves = 0
+  array.each_with_index {|name, index| puts "#{index + 1}. #{name} "}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array) # code an argument here
+  captain_mj = []
+  array.collect do |calls|
+    captain_mj << "#{calls.capitalize}!"
+  end
+  captain_mj
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)  # code an argument here
+  array.any? do |climatechangeisreal|
+    climatechangeisreal.length > 4
+end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(array)
+  array.find do |cheese|
+    cheese == "cheddar" || cheese == "saynotocheese" || cheese == "veganbrieplease"
+  end
 end
